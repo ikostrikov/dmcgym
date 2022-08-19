647208c40f51ff4fbd06a98f5095aa72a0f81abe
Philip Ball <ball@robots.ox.ac.uk>
2022-08-18 22:24:32 -0700

    fix yapf formatting

--- dmcgym/__init__.py
+++ dmcgym/__init__.py
@@ -9,9 +9,11 @@ def create_dm_control_env(domain_name: str, task_name: str) -> gym.Env:
     env = suite.load(domain_name=domain_name, task_name=task_name)
     return DMCGYM(env)
 
+
 def create_dm_control_env_manipulation(environment_name: str) -> gym.Env:
     env = manipulation.load(environment_name=environment_name)
     return DMCGYM(env)
+
 
 create_dm_control_env.metadata = DMCGYM.metadata
 create_dm_control_env_manipulation.metadata = DMCGYM.metadata
@@ -25,8 +27,8 @@ for (domain_name, task_name) in suite.ALL_TASKS:
 for environment_name in manipulation.ALL:
     # only load proprioceptive versions
     if 'features' in environment_name:
-        env_name = environment_name[:-(1+ len('features'))]
+        env_name = environment_name[:-(1 + len('features'))]
         register(id=f"{env_name}-v0",
-                entry_point="dmcgym:create_dm_control_env_manipulation",
-                max_episode_steps=1000,
-                kwargs=dict(environment_name=environment_name))
+                 entry_point="dmcgym:create_dm_control_env_manipulation",
+                 max_episode_steps=1000,
+                 kwargs=dict(environment_name=environment_name))
